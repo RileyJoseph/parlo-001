3f12190fe217ba44e517e46d4a5cdc551e026f67
RileyJoseph <riley.m.joseph1@gmail.com>
2018-10-01 21:35:39 -0500

    first dev commit

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-custom-properties": {},
+    "postcss-custom-media": {},
+    "postcss-mixins": {},
+    "postcss-nested": {},
+    "postcss-clearfix": {},
+    "postcss-svg": {
+      dirs: ['src/svgs']
+    },
+    "postcss-svgo": {},
+    "postcss-short": {},
+    "autoprefixer": {}
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import Components from './components'
+// import Vue from 'vue'
+// import Components from './components'
+// import './app.css'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   el: '#app',
+//   delimiters: ['${', '}']
+// })
+
 import './app.css'
-
-Vue.config.productionTip = false
-
-new Vue({
-  el: '#app',
-  delimiters: ['${', '}']
-})
