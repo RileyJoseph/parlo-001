6756fc03c3eecf46b9a9852fa366a2149710c361
RileyJoseph <riley.m.joseph1@gmail.com>
2019-09-05 18:39:46 -0500

    update css

--- src/main.js
+++ src/main.js
@@ -11,21 +11,21 @@
 
 import './app.css'
 import openNav from './js/openNav.js'
-import { tns } from "../node_modules/tiny-slider/src/tiny-slider"
+// import { tns } from "../node_modules/tiny-slider/src/tiny-slider"
 
 
-var slider = tns({
-    container: '#shop',
-    controlsPosition: "bottom",
-    nav: false,
-    // autoHeight: true,
-    items: 4,
-    gutter: 10,
-    speed: 500,
-    slideBy: 1,
-    // autoplay: true,
-});
+// var slider = tns({
+//     container: '#shop',
+//     controlsPosition: "bottom",
+//     nav: false,
+//     // autoHeight: true,
+//     items: 4,
+//     gutter: 10,
+//     speed: 500,
+//     slideBy: 1,
+//     // autoplay: true,
+// });
 
 openNav();
 
-export default slider
+// export default slider
